2bca9106dae13d8e15a74e52bb05eaf8a4ca23d0
Vinith Prabhu <prabhu.2005.vinith@gmail.com>
2020-10-14 11:11:48 +0530

    Initial Commit
    Cleaned up the react app.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByeW0fRCQlZRpyfOiBOdjy9dMFcZiTRe4",
+    authDomain: "whatsapp-clone-e68e0.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-e68e0.firebaseio.com",
+    projectId: "whatsapp-clone-e68e0",
+    storageBucket: "whatsapp-clone-e68e0.appspot.com",
+    messagingSenderId: "451451263570",
+    appId: "1:451451263570:web:28ca40adbbeb3f49441d8b",
+    measurementId: "G-1VTQDEBQB1"
+  };
